09c62333b3c1aaf93e11801be670e793ce9a95ef
zhou_wei <secret32@sina.com>
2015-09-10 14:37:02 +0800

    add field access flags

--- src/org/sec/dataline/classfile/model/AccessFlags.java
+++ src/org/sec/dataline/classfile/model/AccessFlags.java
@@ -4,7 +4,7 @@ package org.sec.dataline.classfile.model;
  * @author zhou_wei
  * @since v0.1
  */
-public class AccessFlags extends ByteCodeInteger {
+public class AccessFlags {
 
     public static final int ACC_PUBLIC = 0X0001;
     public static final int ACC_FINAL = 0x0010;
@@ -15,12 +15,11 @@ public class AccessFlags extends ByteCodeInteger {
     public static final int ACC_ANNOTATION = 0x2000;
     public static final int ACC_ENUM = 0x4000;
 
-    public AccessFlags() {
-        super(BYTES_ACCESS_FLAGS);
-    }
+    // Field Access Flags
+    public static final int ACC_PRIVATE = 0X0002;
+    public static final int ACC_PROTECTED = 0X0004;
+    public static final int ACC_STATIC = 0X0008;
+    public static final int ACC_VOLATILE = 0X0040;
+    public static final int ACC_TRANSIENT = 0X0080;
 
-    @Override
-    public void visit(Integer value) {
-
-    }
 }
